c589d5b3649f5bc5ea99dfb994d699655ecda3d0
Dmytro Zhluktenko <dmytro.zhluktenko@eleks.com>
2019-10-20 20:47:38 +0300

    Initial

---
+++ variables.tf
@@ -0,0 +1,64 @@
+variable "resource_group_name" {
+  description = "Default resource group name that the database will be created in."
+  default     = "dimko-rg"
+}
+
+variable "location" {
+  default = "westeurope"    
+  description = "The location/region where the database and server are created. Changing this forces a new resource to be created."
+}
+
+variable "server_version" {
+  description = "The version for the database server. Valid values are: 2.0 (for v11 server) and 12.0 (for v12 server)."
+  default     = "12.0"
+}
+
+variable "db_name" {
+  default = "dimko-db"
+  description = "The name of the database to be created."
+}
+
+variable "db_edition" {
+  description = "The edition of the database to be created."
+  default     = "Basic"
+}
+
+variable "service_objective_name" {
+  description = "The performance level for the database. For the list of acceptable values, see https://docs.microsoft.com/en-gb/azure/sql-database/sql-database-service-tiers. Default is Basic."
+  default     = "Basic"
+}
+
+variable "collation" {
+  description = "The collation for the database. Default is SQL_Latin1_General_CP1_CI_AS"
+  default     = "SQL_Latin1_General_CP1_CI_AS"
+}
+
+variable "sql_admin_username" {
+  default = "dimko_sa"
+  description = "The administrator username of the SQL Server."
+}
+# random
+variable "sql_password" {
+  default = "Pass123$"
+  description = "The administrator password of the SQL Server."
+}
+
+variable "start_ip_address" {
+  description = "Defines the start IP address used in your database firewall rule."
+  default     = "0.0.0.0"
+}
+
+variable "end_ip_address" {
+  description = "Defines the end IP address used in your database firewall rule."
+  default     = "255.255.255.255"
+}
+
+variable "tags" {
+  description = "The tags to associate with your network and subnets."
+  type        = "map"
+
+  default = {
+    tag1 = ""
+    tag2 = ""
+  }
+}
